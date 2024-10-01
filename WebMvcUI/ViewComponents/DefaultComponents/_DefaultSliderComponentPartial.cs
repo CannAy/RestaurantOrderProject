0bcca7f5942d24f51bd28a2d94958b24be83fb86
Can Ay <mrtcn.ay@gmail.com>
2024-10-02 00:12:01 +0300

    ..

--- WebMvcUI/ViewComponents/DefaultComponents/_DefaultSliderComponentPartial.cs
+++ WebMvcUI/ViewComponents/DefaultComponents/_DefaultSliderComponentPartial.cs
@@ -1,12 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using WebUI.Dtos.SliderDtos;
 
 namespace WebUI.ViewComponents.DefaultComponents
 {
-	public class _DefaultSliderComponentPartial:ViewComponent
-	{
-		public IViewComponentResult Invoke()
-		{
-			return View();
-		}
-	}
+    public class _DefaultSliderComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultSliderComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7027/api/Sliders");
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultSliderDto>>(jsonData); //listeleme yaptığım için Deserialize.
+            return View(values);
+        }
+    }
 }
