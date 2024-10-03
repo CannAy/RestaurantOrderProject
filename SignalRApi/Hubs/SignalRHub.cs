419393d0e7fdf494fc390e9a1dee965c496d24cf
Can Ay <mrtcn.ay@gmail.com>
2024-10-04 02:24:56 +0300

    Anlık mesajlaşma işlemleri (başlangıç)

--- SignalRApi/Hubs/SignalRHub.cs
+++ SignalRApi/Hubs/SignalRHub.cs
@@ -13,7 +13,7 @@ namespace SignalRApi.Hubs
         private readonly IMoneyCaseService _moneyCaseService;
         private readonly IMenuTableService _menuTableService;
         private readonly IBookingService _bookingService;
-        private readonly INotificationService _notificationService;
+        private readonly INotificationService _notificationService;      
 
 		public SignalRHub(ICategoryService categoryService, IProductService productService, IOrderService orderService, IMoneyCaseService moneyCaseService, IMenuTableService menuTableService, IBookingService bookingService, INotificationService notificationService)
 		{
@@ -76,7 +76,6 @@ namespace SignalRApi.Hubs
             await Clients.All.SendAsync("ReceiveMenuTableCount", value16);
 
         }
-
         public async Task SendProgress()
         {
             var value = _moneyCaseService.TTotalMoneyCaseAmount();
@@ -88,13 +87,11 @@ namespace SignalRApi.Hubs
             var value3 = _menuTableService.TMenuTableCount();
             await Clients.All.SendAsync("ReceiveMenuTableCount", value3);
         }
-
         public async Task GetBookingList()
         {
             var values = _bookingService.TGetListAll();
             await Clients.All.SendAsync("ReceiveBookingList", values);
         }
-
         public async Task SendNotification()
         {
 			var value = _notificationService.TNotificationCountByStatusFalse();
@@ -103,5 +100,14 @@ namespace SignalRApi.Hubs
             var notificationListByFalse=_notificationService.TGetAllNotificationByFalse();
             await Clients.All.SendAsync("ReceiveNotificationListByFalse", notificationListByFalse);
 		}
+        public async Task GetMenuTableStatus()
+        {
+            var value = _menuTableService.TGetListAll();
+            await Clients.All.SendAsync("ReceiveMenuTableStatus", value);
+        }
+        public async Task SendMessage(string user,string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
     }
 }
--- WebMvcUI/Controllers/MenuTablesController.cs
+++ WebMvcUI/Controllers/MenuTablesController.cs
@@ -85,5 +85,19 @@ namespace WebUI.Controllers
 			}
 			return View();
 		}
-	}
+
+		[HttpGet]
+        public async Task<IActionResult> TableListByStatus()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7027/api/MenuTables");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultMenuTableDto>>(jsonData); //listeleme yaptığım için Deserialize.
+                return View(values);
+            }
+            return View();
+        }
+    }
 }
